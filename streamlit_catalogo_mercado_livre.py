37828a56a6249916f7d9afcc05f9ee9d2642ea01
Pedro Dellazzari <64543476+Pedro-Dellazzari@users.noreply.github.com>
2021-04-26 15:44:45 -0300

    Update streamlit_catalogo_mercado_livre.py

--- streamlit_catalogo_mercado_livre.py
+++ streamlit_catalogo_mercado_livre.py
@@ -10,7 +10,7 @@ import base64
 def get_dataset(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}">Download</a>
+    href = f'<a href="data:file/csv;base64,{b64}">Download</a>'
     
     return href
 
